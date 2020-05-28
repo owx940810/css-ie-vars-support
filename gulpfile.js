65c5e52f2895b2fe058f03d6a238e598804d540f
Calvin <calvin@pigeonlab.com>
2020-05-28 14:45:07 +0800

    Remain as ES6 for babel

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
 const { parallel, src, dest } = require('gulp')
 const rename = require('gulp-rename')
 const uglify = require('gulp-uglify')
+const babel = require('gulp-babel')
 
 function minJs () {
   return src('src/index.js')
+    .pipe(babel({
+      presets: ['@babel/env']
+    }))
     .pipe(uglify())
     .pipe(rename((path) => {
       path.basename = 'css-ie-vars-support'
--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 /* global XMLHttpRequest */
 // this code is a replicate of the answer from https://stackoverflow.com/a/47097746
 
-var cssVarPoly = {
+const cssVarPoly = {
   init: function () {
     // first lets see if the browser supports CSS variables
     // No version of IE supports window.CSS.supports, so if that isn't supported in the first place we know CSS variables is not supported
-
     // Edge supports supports, so check for actual variable support
     if (window.CSS && window.CSS.supports && window.CSS.supports('(--foo: red)')) {
       // this browser does support variables, abort
       return
     } else {
+      // edge barfs on console statements if the console is not open... lame!
       document.querySelector('body').classList.add('cssvars-polyfilled')
     }
 
@@ -25,14 +25,14 @@ var cssVarPoly = {
 
   // find all the css blocks, save off the content, and look for variables
   findCSS: function () {
-    var styleBlocks = document.querySelectorAll('style:not(.inserted),link[type="text/css"]')
+    const styleBlocks = document.querySelectorAll('style:not(.inserted),link[type="text/css"]')
 
     // we need to track the order of the style/link elements when we save off the CSS, set a counter
-    var counter = 1;
+    let counter = 1;
 
     // loop through all CSS blocks looking for CSS variables being set
     [].forEach.call(styleBlocks, function (block) {
-      var theCSS
+      let theCSS
       if (block.nodeName === 'STYLE') {
         theCSS = block.innerHTML
         cssVarPoly.findSetters(theCSS, counter)
@@ -61,8 +61,8 @@ var cssVarPoly = {
     cssVarPoly.ratifySetters(cssVarPoly.varsByBlock)
 
     // loop through the css blocks (styles and links)
-    for (var curCSSID in cssVarPoly.oldCSS) {
-      var newCSS = cssVarPoly.replaceGetters(cssVarPoly.oldCSS[curCSSID], cssVarPoly.ratifiedVars)
+    for (const curCSSID in cssVarPoly.oldCSS) {
+      const newCSS = cssVarPoly.replaceGetters(cssVarPoly.oldCSS[curCSSID], cssVarPoly.ratifiedVars)
       // put it back into the page
       // first check to see if this block exists already
       if (document.querySelector('#inserted' + curCSSID)) {
@@ -80,14 +80,14 @@ var cssVarPoly = {
 
   // parse a provided block of CSS looking for a provided list of variables and replace the --var-name with the correct value
   replaceGetters: function (curCSS, varList) {
-    for (var theVar in varList) {
+    for (const theVar in varList) {
       // match the variable with the actual variable name
-      var getterRegex = new RegExp('var\\(\\s*' + theVar + '\\s*\\)', 'g')
+      const getterRegex = new RegExp('var\\(\\s*' + theVar + '\\s*\\)', 'g')
       curCSS = curCSS.replace(getterRegex, varList[theVar])
 
       // now check for any getters that are left that have fallbacks
-      var getterRegex2 = new RegExp('var\\(\\s*.+\\s*,\\s*(.+)\\)', 'g')
-      var matches = curCSS.match(getterRegex2)
+      const getterRegex2 = new RegExp('var\\(\\s*.+\\s*,\\s*(.+)\\)', 'g')
+      const matches = curCSS.match(getterRegex2)
       if (matches) {
         matches.forEach(function (match) {
           // find the fallback within the getter
@@ -103,12 +103,12 @@ var cssVarPoly = {
   // determine the css variable name value pair and track the latest
   ratifySetters: function (varList) {
     // loop through each block in order, to maintain order specificity
-    for (var curBlock in varList) {
-      var curVars = varList[curBlock]
+    for (const curBlock in varList) {
+      const curVars = varList[curBlock]
       // loop through each var in the block
       curVars.forEach(function (theVar) {
         // split on the name value pair separator
-        var matches = theVar.split(/:\s*/)
+        const matches = theVar.split(/:\s*/)
         // put it in an object based on the varName. Each time we do this it will override a previous use and so will always have the last set be the winner
         // 0 = the name, 1 = the value, strip off the ; if it is there
         cssVarPoly.ratifiedVars[matches[0]] = matches[1].replace(/;/, '')
@@ -118,7 +118,7 @@ var cssVarPoly = {
 
   // get the CSS file (same domain for now)
   getLink: function (url, counter, success) {
-    var request = new XMLHttpRequest()
+    const request = new XMLHttpRequest()
     request.open('GET', url, true)
     request.overrideMimeType('text/css;')
     request.onload = function () {
